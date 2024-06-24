6e43e5c6557f94e1405876da2b0e6173b1217a8b
leoxhwang <1134086740@qq.com>
2024-06-24 22:29:16 +0800

    feat: :sparkles: update setup.py

--- smac_pettingzoo/setup.py
+++ smac_pettingzoo/setup.py
@@ -1,6 +1,26 @@
 from __future__ import absolute_import, division, print_function
 
+import os
+import subprocess
+
 from setuptools import find_packages, setup
+
+
+# Function to update submodules
+def update_submodules():
+    if os.path.isdir(".git"):
+        try:
+            subprocess.check_call(["git", "submodule", "update", "--init", "--recursive"])
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to update submodules: {e}")
+        else:
+            print("Submodules updated successfully.")
+    else:
+        print("Not a git repository.")
+
+
+# Call the function to update submodules
+update_submodules()
 
 description = """SMAC-PettingZoo - Latest StarCraft Multi-Agent Challenge. The origin environment can be found at https://github.com/oxwhirl/smac.git and https://github.com/oxwhirl/smacv2.git."""
 
